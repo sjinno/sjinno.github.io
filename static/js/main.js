3302eb607a94293298061ee10e7ddcbda06780a7
Shohei Jinno <shohei.jinno@gmail.com>
2021-08-29 00:31:03 -0700

    Organize ts files.

--- static/js/main.js
+++ static/js/main.js
@@ -1,30 +1,32 @@
-'use strict';
-var hamburger = document.querySelector('.hamburger');
-hamburger.addEventListener('click', function (evt) {
-    var nav = document.querySelector('.nav');
-    var hamburgerClose =
-        '<a href="#" class="hamburger__btn hamburger--close"><span class="hamburger__btn__icon">&times;</span></a>';
-    var hamburgerOpen =
-        '<a href="#" class="hamburger__btn hamburger--open"><span class="hamburger__btn__icon">三</span></a>';
-    var target = evt.target;
-    if (target.classList[1] === 'hamburger--open') {
-        hamburger.innerHTML = hamburgerClose;
-        nav.style.visibility = 'visible';
-        nav.style.opacity = '1';
-    } else if (target.classList[1] === 'hamburger--close') {
-        hamburger.innerHTML = hamburgerOpen;
-        nav.style.opacity = '0';
-        nav.style.visibility = 'hidden';
-    }
-});
-window.onclick = function (evt) {
-    var nav = document.querySelector('.nav');
-    var hamburgerOpen =
-        '<a href="#" class="hamburger__btn hamburger--open"><span class="hamburger__btn__icon">三</span></a>';
-    var target = evt.target;
-    if (target.className === 'nav') {
-        hamburger.innerHTML = hamburgerOpen;
-        nav.style.opacity = '0';
-        nav.style.visibility = 'hidden';
-    }
-};
+"use strict";
+const hamburger = document.querySelector('.hamburger');
+if (hamburger) {
+    hamburger.addEventListener('click', (evt) => {
+        const nav = document.querySelector('.nav');
+        const hamburgerClose = '<a href="#" class="hamburger__btn hamburger--close"><span class="hamburger__btn__icon">&times;</span></a>';
+        const hamburgerOpen = '<a href="#" class="hamburger__btn hamburger--open"><span class="hamburger__btn__icon">三</span></a>';
+        const target = evt.target;
+        if (target.classList[1] === 'hamburger--open') {
+            hamburger.innerHTML = hamburgerClose;
+            nav.style.visibility = 'visible';
+            nav.style.opacity = '1';
+        }
+        else if (target.classList[1] === 'hamburger--close') {
+            hamburger.innerHTML = hamburgerOpen;
+            nav.style.opacity = '0';
+            nav.style.visibility = 'hidden';
+        }
+    });
+    window.onclick = (evt) => {
+        const nav = document.querySelector('.nav');
+        const hamburgerOpen = '<a href="#" class="hamburger__btn hamburger--open"><span class="hamburger__btn__icon">三</span></a>';
+        const target = evt.target;
+        if (target) {
+            if (target.className === 'nav') {
+                hamburger.innerHTML = hamburgerOpen;
+                nav.style.opacity = '0';
+                nav.style.visibility = 'hidden';
+            }
+        }
+    };
+}
---
+++ ts/main.ts
@@ -0,0 +1,38 @@
+const hamburger = document.querySelector('.hamburger');
+
+if (hamburger) {
+    hamburger.addEventListener('click', (evt: Event) => {
+        const nav = document.querySelector('.nav') as HTMLElement;
+        const hamburgerClose: string =
+            '<a href="#" class="hamburger__btn hamburger--close"><span class="hamburger__btn__icon">&times;</span></a>';
+        const hamburgerOpen: string =
+            '<a href="#" class="hamburger__btn hamburger--open"><span class="hamburger__btn__icon">三</span></a>';
+
+        const target = evt.target as HTMLButtonElement;
+        if (target.classList[1] === 'hamburger--open') {
+            hamburger.innerHTML = hamburgerClose;
+            nav.style.visibility = 'visible';
+            nav.style.opacity = '1';
+        } else if (target.classList[1] === 'hamburger--close') {
+            hamburger.innerHTML = hamburgerOpen;
+            nav.style.opacity = '0';
+            nav.style.visibility = 'hidden';
+        }
+    });
+
+    window.onclick = (evt: Event) => {
+        const nav = document.querySelector('.nav') as HTMLElement;
+        const hamburgerOpen: string =
+            '<a href="#" class="hamburger__btn hamburger--open"><span class="hamburger__btn__icon">三</span></a>';
+        const target = evt.target as HTMLElement;
+        if (target) {
+            if (target.className === 'nav') {
+                hamburger.innerHTML = hamburgerOpen;
+                nav.style.opacity = '0';
+                nav.style.visibility = 'hidden';
+            }
+        }
+    };
+}
+
+
